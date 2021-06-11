eecb76990f74b8944b52f7529a0915ef2ad38a47
madhouse <35741027+m4dhouse@users.noreply.github.com>
2021-06-11 22:56:10 +0200

    Update mbflashonline.sh

--- mbflashonline.sh
+++ mbflashonline.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+clear
 echo "check_certificate = off" >> ~/.wgetrc
 opkg --force-reinstall --force-overwrite --force-downgrade install https://github.com/m4dhouse/Multiboot-FlashOnline/blob/main/enigma2-plugin-extensions-multiboot-flashonline_2.8_all.ipk?raw=true
 exit 0
